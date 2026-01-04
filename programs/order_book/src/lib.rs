64ba029a1431ac6be3f240a9e1b33e38d83d72c9
vikas <mvikasreddy123@gmail.com>
2026-01-04 10:06:49 +0530

    added match order

--- programs/order_book/src/lib.rs
+++ programs/order_book/src/lib.rs
@@ -11,7 +11,7 @@ mod state;
 #[program]
 pub mod order_book {
     use super::*;
-    use crate::instructions::{buy_order::{BuyOrderLimit,buy_order},init_market::{InitializeMarket,initilize_market}};
+    use crate::instructions::{buy_order::{BuyOrderLimit,buy_order},init_market::{InitializeMarket,initilize_market},match_order::{MatchOrder,match_order}};
 
     pub fn init_market(ctx: Context<InitializeMarket>,base_mint:Pubkey,quote_mint:Pubkey)->Result<()> {
         initilize_market(ctx,base_mint,quote_mint)
@@ -23,5 +23,9 @@ pub mod order_book {
 
     pub fn limit_sell_order(ctx: Context<SellOrderLimit>, id: u64,quantity: u64,sell_price: u64,base_mint: Option<Pubkey>,quote_mint: Option<Pubkey>) -> Result<()> {
         sell_order(ctx, id,quantity,sell_price,base_mint,quote_mint)
+    }
+
+    pub fn make_transfer(ctx: Context<MatchOrder>,buy_order_id:u64,sell_order_id:u64)->Result<()> {
+        match_order(ctx,buy_order_id,sell_order_id)
     }
 }
